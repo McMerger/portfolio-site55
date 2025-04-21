383ae4a03a05f1f5cf1a461a7af9f29f32441357
McMerger <cortesmailles01@gmail.com>
2025-04-21 16:56:13 -0400

    init commit

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <main style={{ textAlign: "center", padding: "5rem" }}>
+        <h1>Mailles Cortês Portfolio</h1>
+        <p>Full-stack dev. Finance brain. Cold execution.</p>
+      </main>
+    )
+  }  
